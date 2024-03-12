490f3ee2991489850463e493cf6f425facf966ba
Jeremiah <jeremherring@gmail.com>
2024-03-12 13:53:03 -0700

    connect to database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express();
 app.use(express.json())
 
+const dbURI = process.env.DB_URL
 
+mongoose
+  .connect(dbURI)
+  .then((result) =>
+    app.listen(3000, (req, res) => {
+      console.log("Connected to DB listening on port 3000");
+    })
+  )
+  .catch((error) => console.log(error));
